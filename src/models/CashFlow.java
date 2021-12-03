0bda2f522677978229febe4c2f54e08a74d6845b
PedroFi <pedromiguelfilipesimoes@outlook.com>
2021-12-03 15:15:20 +0000

    Created CashFlow

---
+++ src/models/CashFlow.java
@@ -0,0 +1,9 @@
+package models;
+
+public class CashFlow {
+    public DateTime getDate() { return null; }
+
+    public double getValue() { return 0; }
+
+    public CashFlowRate getRate() { return null; }
+}
