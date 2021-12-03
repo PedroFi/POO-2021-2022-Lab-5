ed811fcd937c2ab0459c54d66dd6950a8ec8ab3d
PedroFi <pedromiguelfilipesimoes@outlook.com>
2021-12-03 15:13:01 +0000

    Created Account

---
+++ src/models/Account.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Account {
+    public double getBalance() { return 0; }
+
+    public Client getOwner() { return null; }
+
+    public String getID() { return null; }
+
+    public boolean getAllowDebt() { return null; }
+
+    public Collection<Client> getSharedClients() { return null; }
+
+    public Collection<CashFlow> getCashFlows() { return null; }
+}
