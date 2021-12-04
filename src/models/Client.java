b4e9089c23304fac99f01fd440c893b2509a3687
PedroFi <73402295+PedroFi@users.noreply.github.com>
2021-12-04 21:06:30 +0000

    Completed implementation of Client.

--- src/models/Client.java
+++ src/models/Client.java
@@ -1,46 +1,144 @@
 package models;
 
 import java.util.Collection;
+import java.util.Date;
 
 public class Client implements Comparable<Client> {
-    public String getId() {
-        return null;
+    private String name;
+    private String id;
+    private IdentificationType idTypeSymbol;
+    private Date birthday;
+    private String address;
+    private String email;
+    private String phoneNumber;
+    private Collection<Account> accounts;
+    private Collection<Account> sharedAccounts;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public Client(final String id, final IdentificationType idTypeSymbol) {
+        this.id = id;
+        this.idTypeSymbol = idTypeSymbol;
     }
 
-    public String getIdTypeSymbol() {
-        return null;
+    public Client(final String name, final String id, final IdentificationType idTypeSymbol) {
+        this.name = name;
+        this.id = id;
+        this.idTypeSymbol = idTypeSymbol;
     }
+
+    public Client(final String name, final String id, final IdentificationType idTypeSymbol, final Date birthday) {
+        this.name = name;
+        this.id = id;
+        this.idTypeSymbol = idTypeSymbol;
+        this.birthday = birthday;
+    }
+
+    public Client(final String name, final String id, final IdentificationType idTypeSymbol, final Date birthday, final String address) {
+        this.name = name;
+        this.id = id;
+        this.idTypeSymbol = idTypeSymbol;
+        this.birthday = birthday;
+        this.address = address;
+    }
+
+    public Client(final String name, final String id, final IdentificationType idTypeSymbol, final Date birthday, final String address, final String email) {
+        this.name = name;
+        this.id = id;
+        this.idTypeSymbol = idTypeSymbol;
+        this.birthday = birthday;
+        this.address = address;
+        this.email = email;
+    }
+
+    public Client(final String name, final String id, final IdentificationType idTypeSymbol, final Date birthday, final String address, final String email, final String phoneNumber) {
+        this.name = name;
+        this.id = id;
+        this.idTypeSymbol = idTypeSymbol;
+        this.birthday = birthday;
+        this.address = address;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public String getName() {
-        return null;
+        return name;
     }
 
-    public String getBirthday() {
-        return null;
+    public String getId() {
+        return id;
     }
 
-    public String getEmail() {
-        return null;
+    public IdentificationType getIdTypeSymbol() {
+        return idTypeSymbol;
     }
 
-    public String getPhoneNumber() {
-        return null;
+    public Date getBirthday() {
+        return birthday;
     }
 
     public String getAddress() {
-        return null;
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     public Collection<Account> getAccounts() {
-        return null;
+        return accounts;
+    }
+
+    public boolean addAccount(final Account account) {
+        return this.accounts.add(account);
+    }
+
+    public boolean removeAccount(final Account account) {
+        return this.accounts.remove(account);
     }
 
     public Collection<Account> getSharedAccounts() {
-        return null;
+        return sharedAccounts;
     }
 
+    public boolean addSharedAccount(final Account sharedAccount) {
+        return this.sharedAccounts.add(sharedAccount);
+    }
+
+    public boolean removeSharedAccount(final Account sharedAccount) {
+        return this.sharedAccounts.remove(sharedAccount);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
     @Override
-    public int compareTo(Client e) {
+    public int compareTo(final Client e) {
         return this.getName().compareTo(e.getName());
     }
 }
