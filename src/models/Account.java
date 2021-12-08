14e090ec23076241c6de66bbbd7b7fea167d07af
PedroFi <73402295+PedroFi@users.noreply.github.com>
2021-12-08 21:30:13 +0000

    Updated & Completed Implementation Of Account.

--- src/models/Account.java
+++ src/models/Account.java
@@ -1,20 +1,35 @@
 package models;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Account {
     private double balance;
-    private Client owner; //Final?
+    private final Client owner;
     private final String id;
-    private Collection<Client> sharedClients;
-    private Collection<CashFlow> cashFlows;
+    private final List<Client> sharedClients = new ArrayList<>(80);
+    private final List<CashFlow> cashFlows = new ArrayList<>(80);
+    private boolean allowDept = false;
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public Account(final String id, final Client owner, final double balance = 0.0000) {
+    public Account(final String id, final Client owner) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance; 
+        this.balance = 0.0;
+    }
+
+    public Account(final String id, final Client owner, final double balance) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public Account(final String id, final Client owner, final double balance, final boolean allowDept) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+        this.allowDept = allowDept;
     }
         
     ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -27,22 +42,38 @@ public class Account {
         return id;
     }
 
-    public boolean allowDept(final CashFlow cashFlow) {
-        return balance - cashFlow.value >= 0.0;
+    public List<Client> getSharedClients() {
+        return sharedClients;
     }
 
-    public boolean addSharedClient(final Client client) {
-        return sharedClients.add(client)
+    public void addSharedClient(final Client client) {
+        sharedClients.add(client);
     }
 
-    public boolean removeSharedClient(final Client client) {
-        return sharedClients.remove(client)
+    public void removeSharedClient(final Client client) {
+        sharedClients.remove(client);
     }
 
     public void makeMovement(final CashFlow cashFlow) {
-        if(allowDept(cashFlow)) {
+        if (getBalance() >= cashFlow.getValue() || allowDept()) {
             cashFlows.add(cashFlow);
-            balance += (cashFlow.value - 0.42);
+            balance += (cashFlow.getValue() - 0.42);
         }
+    }
+
+    public Client getOwner() {
+        return owner;
+    }
+
+    public List<CashFlow> getCashFlows() {
+        return cashFlows;
+    }
+
+    public boolean allowDept(){
+        return allowDept;
+    }
+
+    public void setAllowDept(boolean allowDept) {
+        this.allowDept = allowDept;
     }
 }
